18be1a29af442da96101a25e59d4d783a235d529
orozcoarbelaezjesus@gmail.com <orozcoarbealaezjesus@gmail.com>
2021-09-01 18:39:10 -0500

    Proyecto desarrollado en el curso de Java SE de Platzi

---
+++ src/model/Patient.java
@@ -0,0 +1,85 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Patient extends User {
+
+    private String birthday;
+    private double weight;
+    private double height;
+    private String blood;
+
+    private ArrayList<AppointmentDoctor> appointmentDoctors = new ArrayList<>();
+    private ArrayList<AppointmentNurse> appointmentNurses = new ArrayList<>();
+
+    public ArrayList<AppointmentDoctor> getAppointmentDoctors() {
+        return appointmentDoctors;
+    }
+
+    public void addAppointmentDoctors(Doctor doctor, Date date, String time) {
+        AppointmentDoctor appointmentDoctor = new AppointmentDoctor(this, doctor);
+        appointmentDoctor.schedule(date, time);
+        appointmentDoctors.add(appointmentDoctor);
+    }
+
+    public ArrayList<AppointmentNurse> getAppointmentNurses() {
+        return appointmentNurses;
+    }
+
+    public void setAppointmentNurses(ArrayList<AppointmentNurse> appointmentNurses) {
+        this.appointmentNurses = appointmentNurses;
+    }
+
+    public Patient(String name, String email){
+        super(name, email);
+    }
+
+    @Override
+    public void showDataUser() {
+        System.out.println("Cliente");
+        System.out.println("Historia completa del paciente");
+    }
+
+    public String getWeight() {
+        return this.weight + " Kg. ";
+    }
+
+    public void setWeight(double weight){
+        this.weight = weight;
+    }
+
+    public String getHeight() {
+        return this.height + " Mts. ";
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getBlood() {
+        return blood;
+    }
+
+    public void setBlood(String blood) {
+        this.blood = blood;
+    }
+
+    @Override
+    public String toString() {
+        return "model.Patient{" +
+                "birthday='" + birthday + '\'' +
+                ", weight=" + getWeight() +
+                ", height=" + getHeight() +
+                ", blood='" + blood + '\'' +
+                '}';
+    }
+}
